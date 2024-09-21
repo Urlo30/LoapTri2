0e02ad3bfcff429175c99ee7ce11e5835c06ebb4
Urlo30 <91965455+UrloMythus@users.noreply.github.com>
2024-09-21 14:30:17 +0200

    Update run.py

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ main_app = FastAPI()
 
 main_app.router.include_router(mediaflow_app.router)
 @app.get("/wow")
-  return {"message": "This is the main app"}
+async def root():
+    return {"message": "This is the main app"}
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(main_app, host="0.0.0.0", port=8080)
