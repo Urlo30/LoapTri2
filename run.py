0ad93d560a176e26ec61b8cf33534f2e4cffa950
Urlo30 <91965455+UrloMythus@users.noreply.github.com>
2024-09-21 15:10:58 +0200

    Update run.py

--- run.py
+++ run.py
@@ -11,21 +11,21 @@ main_app.router.include_router(mediaflow_app.router)
 @main_app.get("/wow")
 async def root():
     return {"message": "This is the main app"}
-@main_app.get('/huhu/{id}')
-async def huhu(id:str):
+@main_app.get('/huhu/')
+async def huhu(d:str):
     async with httpx.AsyncClient() as client:
-        response = client.get(f"https://huhu.to/play/{id}/index.m3u8")
+        response = client.get(f"https://huhu.to/play/{d}/index.m3u8")
         return response.url
 
 @main_app.get('/mixdrop/')
 async def mixdrop(d:str):
-    print(f"Received ID: {id}")
+    print(f"Received ID: d")
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.10; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
         'Accept-Language': 'en-US,en;q=0.5'
     }
     async with httpx.AsyncClient() as client:
-        response = await client.get(id, headers=headers, follow_redirects=True,timeout = 30)
+        response = await client.get(d, headers=headers, follow_redirects=True,timeout = 30)
         [s1, s2] = re.search(r"\}\('(.+)',.+,'(.+)'\.split", response.text).group(1, 2)
         schema = s1.split(";")[2][5:-1]
         terms = s2.split("|")
