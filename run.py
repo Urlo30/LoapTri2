4102a0528692ad6a9b35337a337a2a5468166964
Urlo30 <91965455+UrloMythus@users.noreply.github.com>
2024-09-21 14:34:00 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, HTTPException,Request
 from mediaflow_proxy.main import app as mediaflow_app
 from importlib import resources
-static_path = resources.files("mediaflow_proxy").joinpath("static")
-main_app.mount("/static", StaticFiles(directory=str(static_path), html=True), name="static")
+
 
 
 main_app = FastAPI()
-
+static_path = resources.files("mediaflow_proxy").joinpath("static")
+main_app.mount("/static", StaticFiles(directory=str(static_path), html=True), name="static")
 main_app.router.include_router(mediaflow_app.router)
 @app.get("/wow")
 async def root():
