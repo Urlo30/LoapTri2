0588e5bcd2979ef042b7cab0f9aedbd0b7bea1c5
Urlo30 <91965455+UrloMythus@users.noreply.github.com>
2024-09-21 14:50:48 +0200

    Update main.py

--- mediaflow_proxy/main.py
+++ mediaflow_proxy/main.py
@@ -48,31 +48,6 @@ async def verify_api_key(api_key: str = Security(api_password_query), api_key_al
 async def health_check():
     return {"status": "healthy"}
 
-@app.get("/huhu/{id}")
-async def huhu():
-    async with httpx.AsyncClient() as client:
-        response = client.get(f"https://huhu.to/play/{id}/index.m3u8")
-        return response.url
-
-@app.get("/mixdrop/{id}")
-async def mixdrop():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.10; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-        'Accept-Language': 'en-US,en;q=0.5'
-    }
-    async with httpx.AsyncClient() as client:
-        response = await client.get(id, headers=headers, follow_redirects=True,timeout = 30)
-        [s1, s2] = re.search(r"\}\('(.+)',.+,'(.+)'\.split", response.text).group(1, 2)
-        schema = s1.split(";")[2][5:-1]
-        terms = s2.split("|")
-        charset = string.digits + string.ascii_letters
-        d = dict()
-        for i in range(len(terms)):
-            d[charset[i]] = terms[i] or charset[i]
-        s = 'https:'
-        for c in schema:
-            s += d[c] if c in d else c
-        return s
 
 @app.get("/favicon.ico")
 async def get_favicon():
